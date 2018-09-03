6f38c7dd26230f9be3190ba57866614b93235788
Dev <oronmeller@gmail.com>
2018-09-04 02:28:57 +0300

    new changes

---
+++ BL/Utils/consts.js
@@ -0,0 +1,14 @@
+const PULSE_HOLDER = "pulse-component can-edit"
+const PULSE_FIRST_CELL_HOLDER= "cell-component name-cell"
+// const PULSE_NAME_CELL_HOLDER = "name-cell-component"
+const PULSE_NAME_TEXT_HOLDER = "name-text"
+const BOARD_CONTENT_COMPONENT = "board-content-component"
+
+// const IMG_URL_PLAY = chrome.extension.getURL("images/play.png");
+// const stopIIMG_URL_STOPmgUrl = chrome.extension.getURL("images/stop.png");
+
+
+const CLASS_EXTENSION_BTN_WRAPPER = "extension-toggle-btn-wrapper";
+const CLASS_EXTENSION_BTN_CLASS = "extension-toggle-btn";
+const CLASS_EXTENSION_PLAY = "extension-play-btn"
+const CLASS_EXTENSION_STOP = "extension-stop-btn"
--- BL/contentScript.js
+++ BL/contentScript.js
@@ -1,61 +1,82 @@
-const pulseIdHolders = "cell-component name-cell"
-const pulseInjectionLocation = "name-cell-component"
-const pulseNameHolders = "name-text"
 
-const playImgUrl = chrome.extension.getURL("images/play.png");
-const stopImgUrl = chrome.extension.getURL("images/stop.png");
+let lastPlayedPulseId = -1;
 
-let lastPlayedPulesId = 0;
+let observer = new MutationObserver(function(mutations) {
+    if (mutations[1] && mutations[1].addedNodes[0] && mutations[1].addedNodes[0].className.indexOf(PULSE_HOLDER) !== -1) {
+        cellComponentElement = mutations[1] && mutations[1].addedNodes[0].childNodes[1]; //div class="cell-component name-cell"
+        constructSingleToggleButton(cellComponentElement);
+    }
+    // observer.disconnect();
+ });
 
-chrome.storage.sync.get('lastPlayedPulseId', function(data) {
+chrome.storage.sync.get(["lastPlayedPulseId"], function(data) {
     console.log("data is:", data);
     if (Object.keys(data).length === 0 && data.constructor === Object) {
-        chrome.storage.sync.set({lastPlayedPulseId: lastPlayedPulesId});
+        chrome.storage.sync.set({lastPlayedPulseId: lastPlayedPulseId});
     } else {
-        lastPlayedPulesId = data.lastPlayedPulseId
+        lastPlayedPulseId = data.lastPlayedPulseId
     }
+    console.log("data is:", lastPlayedPulseId);
 });
 
 window.onload = function() {
-    let pulseLeftIndicators = document.getElementsByClassName(pulseInjectionLocation);
-    let pulseIds = document.getElementsByClassName(pulseIdHolders);
-    let pulseNames = document.getElementsByClassName(pulseNameHolders);
-
-    // console.log(pulseIds);
-    // console.log(pulseNames);
+    let pulsesCells = document.getElementsByClassName(PULSE_FIRST_CELL_HOLDER);
 
     setTimeout(() => {
-        constructToggleButtons(pulseLeftIndicators, pulseIds, pulseNames);
+        constructMultipleToggleButtons(pulsesCells);
     }, 500);
-    
+
+    observer.observe(document.getElementsByClassName(BOARD_CONTENT_COMPONENT)[0], {attributes: false, childList: true, characterData: false, subtree:true});
 };
 
-function constructToggleButtons(pulsesArray, pulseIdsArray, pulseNamesArray) {
+function constructMultipleToggleButtons(pulsesArray) {
     for (let i = 0; i < pulsesArray.length; i++) {
-        //create element and add custom btn classes
-        let toggleBtn = document.createElement('a');
-        toggleBtn.classList.add("compie-toggle-btn", "compie-play-btn");
-        toggleBtn.id = parsePulseId(pulseIdsArray[i]);
-        toggleBtn.setAttribute("data-pulse-name", pulseNamesArray[i].getElementsByTagName('span')[0].innerText)
-
-        toggleBtn.addEventListener('click', function(e) {
-            toggleButtonImg(this);
-            lastPlayedPulesId = this.id;
-            return false;
-        });
-
-        pulsesArray[i].prepend(toggleBtn);
+        constructSingleToggleButton(pulsesArray[i])
     }
 }
 
+function constructSingleToggleButton(pulseElement) {
+    //create element and add custom btn classes
+    let toggleBtnWrapper = document.createElement('div');
+    let toggleBtn = document.createElement('a');
+    toggleBtnWrapper.classList.add(CLASS_EXTENSION_BTN_WRAPPER)
+    toggleBtn.classList.add(CLASS_EXTENSION_BTN, CLASS_EXTENSION_PLAY);
+    toggleBtn.id = parsePulseId(pulseElement);
+    if (toggleBtn.id == lastPlayedPulseId ) {
+        toggleButtonImg(toggleBtn)
+    }
+    toggleBtn.setAttribute("data-pulse-name", pulseElement.getElementsByClassName(PULSE_NAME_TEXT_HOLDER)[0].getElementsByTagName('span')[0].innerText)
+    toggleBtn.addEventListener('click', function(e) {
+        let hasClass = elementAlreadyPlaying(this);
+        if (hasClass) {
+            lastPlayedPulseId = -1;
+        } else {
+            let lastActivePulse = document.getElementById(lastPlayedPulseId);
+            lastActivePulse && toggleButtonImg(lastActivePulse);
+            lastPlayedPulseId = this.id;
+        }
+        toggleButtonImg(this)
+        chrome.storage.sync.set({lastPlayedPulseId: lastPlayedPulseId}, function() {
+            console.log("storage updated lastPlayedPulseId with id", lastPlayedPulseId)
+        });
+    });
+
+    toggleBtnWrapper.appendChild(toggleBtn)
+    pulseElement.prepend(toggleBtnWrapper);
+}
+
 function toggleButtonImg(element) {
-    // let lastPlayedPulse = document.getElementById(lastPlayedPulesId)
-    // lastPlayedPulse != undefined && element.id != lastPlayedPulesId ? lastPlayedPulse.classList.toggle("compie-stop-btn") : null;
-    element.classList.toggle("compie-stop-btn");
+    element.classList.toggle(CLASS_EXTENSION_STOP);
+}
+
+function elementAlreadyPlaying(element) {
+    return element.className.indexOf(CLASS_EXTENSION_STOP) !== -1;
 }
 
 function parsePulseId(element) {
     return element.id.split('-')[1];
 }
+
+
 
 
